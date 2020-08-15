ddc6369f9e11a53243082efc29747c57aaf06ae8
Tim <tim@veticus.com>
2020-08-16 01:36:25 +0200

    Update hasthisbeenpwned.py

--- hasthisbeenpwned.py
+++ hasthisbeenpwned.py
@@ -11,7 +11,6 @@ import os
 import hashlib
 from pyspin.spin import make_spin, Box1
 import argparse
-import sys
 
 
 def make_uppercase_hash_string(input):
@@ -35,10 +34,7 @@ def make_uppercase_hash_string(input):
 def process_for_tty(filename, string_to_find):
 	# Here we step through the dump file, looking for a match.
 	# First we take our string_to_find and get it converted to a hash string.
-	print("Password to look for: " + string_to_find)
 	hash_to_find = make_uppercase_hash_string(string_to_find)
-	print("Hash of the password: " + hash_to_find)
-	print("Filename of dumpfile: " + filename)
 
 	# Next we open the dump file read-only
 	with open(filename, 'r') as dump:
@@ -50,25 +46,9 @@ def process_for_tty(filename, string_to_find):
 				# This way we will only get a string with the number of occurences.
 				print("\n Number of occurences: " + str(line).split(":")[1])
 				quit()
+	
 	# In case there are no matches, we just print a fitting message, and break out of this function.
 	print("\n Found 0 occurences of this password")
-
-
-def process_for_nontty(filename, string_to_find):
-	# Here we process it, as if it weren't  a tty. Meaning we only return the number of occurences.
-	# First we take our string_to_find and get it converted to a hash string.
-	hash_to_find = make_uppercase_hash_string(string_to_find)
-
-	# Next we open the dump file read-only
-	with open(filename, 'r') as dump:
-		# Now we'll go through each line in the dump file, looking for a match with our hash_to_find.
-		for line in dump:
-			if str(hash_to_find).upper() in line:
-				# If there is a match, we immediately return the number after the colon (:),
-				#   on the line, the hash was found.
-				return str(line).split(":")[1]
-	# We'll reach this line of there is no match. We therefore simply return a 0.
-	return 0
 
 
 if __name__ == '__main__':
@@ -84,17 +64,15 @@ if __name__ == '__main__':
 	arg_string_to_check = args.input
 	arg_filename = args.filename
 
-	# Next we determine if this script is called from a tty.
-	# If it is, we can give the user "pretty output.
-	# If it's not, the scripts output is probably being piped to another program. So we'll just return the result.
-	#
-	# Additionally we'll wrap the whole thing in a try/except to handle KeyboardInterrupt and SystemExit (ctrl+c)
+	# Now we'll wrap the whole thing in a try/except to handle KeyboardInterrupt and SystemExit (ctrl+c)
 	# We'll specifically use os._exit(1) to give a return code of 1, because the "with open()" thingy locks the thread.
 	# If this isn't dealth with by os._exit(), and exception will be thrown, because the lock termination isn't handled.
+	# We also give the user a little information about what's happening, and a little spinner, to show "progress".
 	try:
-		if sys.stdout.isatty():
-			process_for_tty(arg_filename, arg_string_to_check)
-		else:
-			process_for_nontty(arg_filename, arg_string_to_check)
+		print("Password to look for: " + arg_string_to_check)
+		hash_to_find = make_uppercase_hash_string(arg_string_to_check)
+		print("Hash of the password: " + hash_to_find)
+		print("Filename of dumpfile: " + arg_filename)
+		process_for_tty(arg_filename, arg_string_to_check)
 	except (KeyboardInterrupt, SystemExit):
 		os._exit(1)
